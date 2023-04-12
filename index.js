294c3ef121cb3089611a136ec7789b3a132b7867
gomex500 <gomezfreddy886@gmail.com>
2023-04-11 20:26:53 -0600

    prueba y finalizacion de los ejecicios

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ var add  = (x,y) => {
 
 console.log(add(12,12));
 
+// R= 24
+
+
 
 
 // 9. Escribe una función que tome un arreglo como parámetro y devuelva el número de elementos en el arreglo.
+var size = (cadena) => {
+    let e =0, arreglo = cadena.split("");
+    for (let i = 0; i < arreglo.length; i++) {
+        e++;
+    }
+    return e;
+    // return cadena.length;
+}
 
+console.log(size("Freddy"));
+
+// R= 6
